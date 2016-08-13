303ec2c4239ed764964aa45835fb9ec55e6418e4
gancheev <insanee8@gmail.com>
2016-08-14 02:55:00 +0300

    Fixing bugs/ adding expense information in the index table.

--- ExpenseManager/Controllers/HomeController.cs
+++ ExpenseManager/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using ExpenseManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace ExpenseManager.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var expenses = db.Expenses.Include(e => e.Category).Include(p => p.User);
+            return View(expenses.ToList());
         }
 
         public ActionResult About()
