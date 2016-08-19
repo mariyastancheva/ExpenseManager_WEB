fcb336e03d2de869f696f2ae32dedfeedd7865bb
mariyastancheva <mariq_5151@abv.bg>
2016-08-19 16:59:56 +0300

    Three more graphics added - yearly, monthly,daily basis

--- ExpenseManager/Scripts/morris-data.js
+++ ExpenseManager/Scripts/morris-data.js
@@ -1,31 +1,28 @@
 $(function() {
 
- 
-
     Morris.Donut({
         element: 'morris-donut-chart',
-        data: data,
+        data: dataTotal,
         resize: true
     });
    
     Morris.Donut({
         
         element: 'morris-donut-chart2',
-        data: [{
-            label: "COCAINE",
-            value: 12
-        }, {
-            label: "HASH",
-            value: 30
-        }, {
-            label: "AMPHETAMINE",
-            value: 20
-        }, {
-            label: "KOKA-KOLA",
-            value: 13
-        }
-        ],
+        data: dataFromToday,
         resize: true
     });
-    
+    Morris.Donut({
+
+        element: 'morris-donut-chart3',
+        data: dataFromThisMonth,
+        resize: true
+    });
+
+    Morris.Donut({
+
+        element: 'morris-donut-chart4',
+        data: dataFromThisYear,
+        resize: true
+    });
 });
--- ExpenseManager/Utils/ChartsData.cs
+++ ExpenseManager/Utils/ChartsData.cs
@@ -11,9 +11,35 @@ namespace ExpenseManager.Utils
         public static Dictionary<string, decimal> GetTotalData(IEnumerable<Expense> expenses)
         {
             List<Expense> expensesList = expenses.ToList();
+            var allCategoriesAndExpenses = CalculatingCategoriesAndPrices(expensesList);
+            return allCategoriesAndExpenses;
+        }
+
+        public static Dictionary<string, decimal> GetTodaysData(IEnumerable<Expense> expenses)
+        {
+            List<Expense> expensesList = expenses.Where(e=>e.Date==DateTime.Today).ToList();
+            var allCategoriesAndExpenses = CalculatingCategoriesAndPrices(expensesList);
+            return allCategoriesAndExpenses;
+        }
+        public static Dictionary<string, decimal> GetDataFromThisMonth(IEnumerable<Expense> expenses)
+        {
+            List<Expense> expensesList = expenses.Where(e=>e.Date.Month==DateTime.Now.Month).ToList();
+            var allCategoriesAndExpenses = CalculatingCategoriesAndPrices(expensesList);
+            return allCategoriesAndExpenses;
+        }
+
+        public static Dictionary<string, decimal> GetDataFromThisYear(IEnumerable<Expense> expenses)
+        {
+            List<Expense> expensesList = expenses.Where(e => e.Date.Year == DateTime.Now.Year).ToList();
+            var allCategoriesAndExpenses= CalculatingCategoriesAndPrices(expensesList);
+            return allCategoriesAndExpenses;
+        }
+
+        private static Dictionary<string, decimal> CalculatingCategoriesAndPrices(List<Expense> expList)
+        {
             Dictionary<string, decimal> allCategoriesAndExpenses = new Dictionary<string, decimal>();
-            //Dictionary<string, decimal> categoriesAndExpenses = new Dictionary<string, decimal>();
-            foreach (var e in expensesList)
+
+            foreach (var e in expList)
             {
                 if (allCategoriesAndExpenses.ContainsKey(e.Category.Title))
                 {
